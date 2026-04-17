827a98558a4f8efefe48d92602008ebdd90ac30e
jsamcovic <jsamcovic1996@gmail.com>
2026-04-17 12:31:38 +0200

    Fix: locale props fix

--- src/2-pages/home.tsx
+++ src/2-pages/home.tsx
@@ -14,7 +14,7 @@ const HomePage = async ({ params }: Props) => {
   const { locale } = params;
 
   const projects = await getAllProjects();
-  const featuredProjects = await getFeaturedProjects();
+  const featuredProjects = await getFeaturedProjects(locale);
 
   const slides = featuredProjects
     .filter((p) => p.coverImage)
