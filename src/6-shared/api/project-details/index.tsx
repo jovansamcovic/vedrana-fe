b4cd35e41c5a4e525335fe573e2d178fabcbf1ef
jsamcovic <jsamcovic1996@gmail.com>
2026-04-19 19:21:45 +0200

    fix locale

--- src/6-shared/api/project-details/index.tsx
+++ src/6-shared/api/project-details/index.tsx
@@ -5,11 +5,16 @@ const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
 export async function getProjectBySlug(slug: string, locale: string = "en"): Promise<Project | null> {
   const res = await fetch(
     `${STRAPI_URL}/api/projects?populate=*&filters[slug][$eq]=${slug}&locale=${locale}`,
-    { next: { revalidate: 60 } }
+    { next: { revalidate: 0 } }
   );
 
   if (!res.ok) throw new Error("Failed to fetch project");
 
   const data: StrapiResponse<Project[]> = await res.json();
+
+  if (!data.data[0] && locale !== "en") {
+    return getProjectBySlug(slug, "en");
+  }
+
   return data.data[0] ?? null;
 }
