9bb1d875f4511befc28e47f84d073db85fc25b45
jsamcovic <jsamcovic1996@gmail.com>
2026-04-21 21:35:15 +0200

    Added contact and publications pages

--- src/1-app/projects/projects-layout.tsx
+++ src/1-app/projects/projects-layout.tsx
@@ -1,11 +1,9 @@
-import { Header } from "@/src/4-features/header";
-
 export default async function ProjectsPage({
+
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
-      <Header />
       <div>{children}</div>;
     </>
   );
--- src/2-pages/contact.tsx
+++ src/2-pages/contact.tsx
@@ -1,9 +1,7 @@
 import { ContactSection } from "../4-features/contact-section";
-import { getAllProjects } from "../6-shared/api/all-projects";
 
 export default async function ContactPage({ params }: { params: { locale: string } }) {
   const { locale } = params;
- const projects = await getAllProjects(locale);
 
   return (
     <main>
